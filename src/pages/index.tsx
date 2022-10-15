fea70c95e80c91f83cdce36de87c20f29d7cad6c
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-15 19:03:26 -0300

    style: use semicolon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <div>
       Hello World
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
