31d6a1d740a34f4128c1600f72dbe82893f20cb4
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-15 21:13:42 -0300

    feat: use Header component on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next';
 
+import Header from '../components/Header';
+
 const Home: NextPage = () => {
   return (
     <div>
-      Hello World
+      <Header />
     </div>
   );
 }
