9116cff04d677bb3c2b8087a023c8c6027f95058
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-16 14:01:21 -0300

    feat: add getAllArticles api service function

--- src/services/api.ts
+++ src/services/api.ts
@@ -7,6 +7,15 @@ export interface Article {
   title: string;
 }
 
+type GetAllArticles = () => Promise<Article[]>;
+
+export const getAllArticles: GetAllArticles = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/articles`);
+  const data = await response.json();
+
+  return data as Article[];
+};
+
 type GetArticles = (page?: number, articlesPerPage?: number) => Promise<Article[]>;
 
 export const getArticles: GetArticles = async (page = 1, articlesPerPage = 10) => {
