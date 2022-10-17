924eeb1c1eab667cadb87ad732c52d8b7e2778c5
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-17 20:34:36 -0300

    feat: add hamburguer menu on mobile screens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,20 @@
+import { useRef } from 'react';
 import Link from 'next/link';
 
 import styles from './Header.module.css';
 
+import { useScreenSize } from '../../hooks';
+
+import HamburguerMenu from '../HamburguerMenu';
+
 const Header = () => {
+  const { width } = useScreenSize();
+  const nav = useRef<HTMLUListElement>(null);
+
+  const handleClassName = () => {
+    nav.current?.classList.toggle(styles.active);
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -10,19 +22,20 @@ const Header = () => {
           <a id={styles.logo}>Rockr Blog</a>
           </Link>
         <nav>
-          <ul className={styles.navList}>
-            <li>
+          <ul ref={nav} className={styles.navList}>
+            <li className={styles.navListItem}>
               <Link passHref href="/">
                 <a className={styles.navItem}>Posts</a>
               </Link>
             </li>
-            <li>
+            <li className={styles.navListItem}>
               <Link passHref href="/contact">
                 <a className={styles.navItem}>Contact</a>
               </Link>
             </li>
           </ul>
         </nav>
+        { width <= 768 && <HamburguerMenu handleMenuToggle={handleClassName} /> }
       </div>
     </header>
   );
