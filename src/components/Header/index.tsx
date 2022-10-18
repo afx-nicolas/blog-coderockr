e6b647580c9aee1846d87e9827ede82edb1d40ff
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-18 15:15:19 -0300

    feat: auto close mobile menu on click nav buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ import { modalContext } from '../../contexts/ModalContext';
 import HamburguerMenu from '../HamburguerMenu';
 
 const Header = () => {
-  const { openModal } = useContext(modalContext);
+  const { openModal, closeModal } = useContext(modalContext);
   const { width } = useScreenSize();
   const nav = useRef<HTMLUListElement>(null);
 
@@ -19,30 +19,36 @@ const Header = () => {
     }
   }, [width]);
 
-
   const handleClassName = () => {
     nav.current?.classList.toggle(styles.active);
+  }
+
+  const closeMobileMenu = () => nav.current?.classList.remove(styles.active);
+
+  const handleOpenModal = () => {
+    closeMobileMenu();
+    openModal();
   }
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <Link passHref href="/">
-          <a id={styles.logo}>Rockr Blog</a>
-          </Link>
+          <a onClick={closeModal} id={styles.logo}>Rockr Blog</a>
+        </Link>
         <nav>
           <ul ref={nav} className={styles.navList}>
             <li className={styles.navListItem}>
               <Link passHref href="/">
-                <a className={styles.navItem}>Posts</a>
+                <a onClick={closeMobileMenu} className={styles.navItem}>Posts</a>
               </Link>
             </li>
             <li className={styles.navListItem}>
-              <button onClick={openModal} className={[styles.contactBtn, styles.navItem].join(' ')}>Contact</button>
+              <button onClick={handleOpenModal} className={[styles.contactBtn, styles.navItem].join(' ')}>Contact</button>
             </li>
             <li className={styles.navListItem}>
               <Link passHref href="/post/new">
-                <a className={[styles.navItem, styles.newPost].join(' ')}>New Post</a>
+                <a onClick={closeMobileMenu} className={[styles.navItem, styles.newPost].join(' ')}>New Post</a>
               </Link>
             </li>
           </ul>
