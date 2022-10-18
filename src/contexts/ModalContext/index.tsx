89cf6ce575b5c419b592eff6e7fb6ae377f87185
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-18 13:49:48 -0300

    feat: add Modal context

---
+++ src/contexts/ModalContext/index.tsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect, useState } from 'react';
+
+interface ModalContextProps {
+  isModalOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export const modalContext = createContext({} as ModalContextProps);
+
+interface ModalContextProviderProps {
+  children: React.ReactNode;
+}
+
+const ModalContextProvider = ({ children }: ModalContextProviderProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
+    }
+  }, [isModalOpen]);
+
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => setIsModalOpen(false);
+
+  const props = {
+    isModalOpen,
+    openModal,
+    closeModal
+  }
+
+  return (
+    <modalContext.Provider value={props}>
+      { children }
+    </modalContext.Provider>
+  );
+}
+
+export default ModalContextProvider;
