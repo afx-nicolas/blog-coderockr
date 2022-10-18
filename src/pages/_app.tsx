375161db110a2c7f80dcccb322f68185afbfd12c
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-18 14:14:22 -0300

    feat: wrap app with ModalContextProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,17 @@ import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
+import ModalContextProvider from '../contexts/ModalContext';
+
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
-    <>
+    <ModalContextProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ModalContextProvider>
   );
 }
 
