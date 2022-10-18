719bd8e50d174ded76867f14f1a6cbfed266c876
Nicolas A. Feitoza <afxnicolas@gmail.com>
2022-10-18 17:29:59 -0300

    feat: add attributes required, value and onChange

--- src/components/InputGroup/index.tsx
+++ src/components/InputGroup/index.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react';
+import { ChangeEvent, useId } from 'react';
 
 import styles from './InputGroup.module.css';
 
@@ -8,17 +8,20 @@ interface InputGroupProps {
   type: InputType;
   placeholder: string;
   label: string;
+  required?: boolean;
+  value: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
-const InputGroup = ({ type, placeholder, label }: InputGroupProps) => {
+const InputGroup = ({ onChange, type, placeholder, label, required, value }: InputGroupProps) => {
   const id = useId();
 
   return (
     <div className={styles.inputGroup}>
       <label className={styles.label} htmlFor={id}>{label}</label>
       { type === 'textarea'
-        ? <textarea className={styles.textarea} id={id} rows={10} placeholder={placeholder} />
-        : <input className={styles.input} id={id} type={type} placeholder={placeholder} />
+        ? <textarea onChange={onChange} className={styles.textarea} id={id} rows={10} placeholder={placeholder} required={!!required} value={value} />
+        : <input onChange={onChange} className={styles.input} id={id} type={type} placeholder={placeholder} required={!!required} value={value} />
       }
     </div>
   );
